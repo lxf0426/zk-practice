1b1a7cb2ddf12501f7f6f3de4c467f33a9320437
lxf <965392461@qq.com>
2018-12-02 20:23:32 +0800

    项目完成

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@
  * @Author: Lxf 
  * @Date: 2018-12-01 09:34:37 
  * @Last Modified by: mikey.zhaopeng
- * @Last Modified time: 2018-12-01 11:00:29
+ * @Last Modified time: 2018-12-02 20:04:30
  */
 
 var gulp = require('gulp');
@@ -21,6 +21,8 @@ var server = require('gulp-webserver');
 
 var swiper = require('./src/data/swiper.json')
 
+var babel = require("gulp-babel");
+
 var fs = require('fs');
 
 var path = require('path');
@@ -29,6 +31,7 @@ var path = require('path');
 gulp.task('default', function() {
     return gulp.src('./src/scss/index.scss')
         .pipe(sass())
+        .pipe(minCss())
         .pipe(gulp.dest('./src/css'))
 })
 
@@ -39,7 +42,7 @@ gulp.task('watch', function() {
 
 //起服务
 gulp.task('server', function() {
-    return gulp.src('src')
+    return gulp.src('build')
         .pipe(server({
             port: 8080,
             open: true,
@@ -54,7 +57,7 @@ gulp.task('server', function() {
                     res.end(JSON.stringify({ code: 0, msg: swiper }))
                 } else {
                     pathname = pathname === '/' ? 'index.html' : pathname;
-                    res.end(fs.readFileSync(path.join(__dirname, 'src', pathname)))
+                    res.end(fs.readFileSync(path.join(__dirname, 'build', pathname)))
                 }
             }
         }))
@@ -62,3 +65,36 @@ gulp.task('server', function() {
 
 //开发环境
 gulp.task('dev', gulp.series('default', 'server', 'watch'))
+
+
+//css
+gulp.task("bCss", function() {
+    return gulp.src('./src/css/*.css')
+        .pipe(minCss())
+        .pipe(gulp.dest('./build/css'))
+})
+
+//js
+gulp.task("bJs", function() {
+    return gulp.src('./src/js/*.js')
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./build/js'))
+})
+
+//copeJs
+gulp.task("bCopeJs", function() {
+    return gulp.src('./src/js/lib/*.js')
+        .pipe(gulp.dest('./build/js/lib'))
+})
+
+//html
+gulp.task("bHtml", function() {
+    return gulp.src('./src/*.html')
+        .pipe(gulp.dest('./build/'))
+})
+
+//线上环境
+gulp.task('build', gulp.parallel('bCss', 'bJs', 'bCopeJs', 'bHtml', 'server'))
